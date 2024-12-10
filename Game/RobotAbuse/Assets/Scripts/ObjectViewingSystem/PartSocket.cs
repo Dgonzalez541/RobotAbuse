27a00d242a2b90cc66ff6240997ac6563c7d4bce
David <dgonzalez541@gmail.com>
2024-12-09 20:28:18 -0600

    Cleanup and comment

--- Game/RobotAbuse/Assets/Scripts/ObjectViewingSystem/PartSocket.cs
+++ Game/RobotAbuse/Assets/Scripts/ObjectViewingSystem/PartSocket.cs
@@ -1,27 +1,25 @@
 using System;
-using System.Diagnostics.Tracing;
-using System.Linq;
 using UnityEngine;
 
 namespace RobotAbuse
 {
+
     [RequireComponent(typeof(SphereCollider))]
     [RequireComponent(typeof(Rigidbody))]
     public class PartSocket : MonoBehaviour
     {
-        public event EventHandler OnSocketPartsConnected;
-
         public PartSocket AttachedPartSocket { get; private set; }
-
-        public ObjectViewer ObjectViewer;
-
         public bool IsConnected { get; private set; } = true;
 
         public IViewableObject SocketOwner { get; private set; }
-        
+
+        public ObjectViewer ObjectViewer;
+
+        public event EventHandler OnSocketPartsConnected;
 
         private void Awake()
         {
+            //Ensure collider and Rigidbody work properly
             GetComponent<SphereCollider>().isTrigger = true;
             GetComponent<Rigidbody>().useGravity = false;
 
@@ -49,25 +47,14 @@ namespace RobotAbuse
             ObjectViewer.OnHideAllSockets += ObjectViewer_OnHideAllSockets;
         }
 
-        private void ObjectViewer_OnHideAllSockets(object sender, EventArgs e)
+        private void OnTriggerEnter(Collider other)
         {
-            HideSocket();
-        }
-
-        private void ObjectViewer_OnSocketDetach(object sender, EventArgs e)
-        {
-            var eventArgs = e as OnSocketPartsInteractionEventArgs;
-            if (IsConnected && eventArgs.OtherPartSocket == this)
-            {  
-                IsConnected = false;
+            if (other.gameObject.GetComponent<PartSocket>() != null)
+            {
+                IsConnected = true;
+                AttachedPartSocket = other.gameObject.GetComponent<PartSocket>();
+                OnSocketPartsConnected?.Invoke(this, new OnSocketPartsInteractionEventArgs { GrabbedPartSocket = this, OtherPartSocket = other.gameObject.GetComponent<PartSocket>() }); ;
             }
-                ShowSocket();
-        }
-
-        private void ShowSocket()
-        {
-            var renderer = GetComponent<Renderer>();
-            renderer.material.SetFloat("_Opacity", .5f);
         }
 
         private void ObjectViewer_OnSocketAttach(object sender, EventArgs e)
@@ -81,26 +68,38 @@ namespace RobotAbuse
             HideSocket();
         }
 
+        private void ObjectViewer_OnSocketDetach(object sender, EventArgs e)
+        {
+            var eventArgs = e as OnSocketPartsInteractionEventArgs;
+            if (IsConnected && eventArgs.OtherPartSocket == this)
+            {
+                IsConnected = false;
+            }
+            ShowSocket();
+        }
+
+        private void ShowSocket()
+        {
+            var renderer = GetComponent<Renderer>();
+            renderer.material.SetFloat("_Opacity", .5f);
+        }
+
         private void HideSocket()
         {
             var renderer = GetComponent<Renderer>();
             renderer.material.SetFloat("_Opacity", 0f);
         }
 
-        private void OnTriggerEnter(Collider other)
+        private void ObjectViewer_OnHideAllSockets(object sender, EventArgs e)
         {
-            if(other.gameObject.GetComponent<PartSocket>() != null)
-            {
-                IsConnected = true;
-                AttachedPartSocket = other.gameObject.GetComponent<PartSocket>();
-                OnSocketPartsConnected?.Invoke(this, new OnSocketPartsInteractionEventArgs { GrabbedPartSocket = this, OtherPartSocket = other.gameObject.GetComponent<PartSocket>() }); ;
-            }
+            HideSocket();
         }
 
         private void OnDisable()
         {
             ObjectViewer.OnSocketDetach -= ObjectViewer_OnSocketDetach;
             ObjectViewer.OnSocketAttach -= ObjectViewer_OnSocketAttach;
+            ObjectViewer.OnHideAllSockets -= ObjectViewer_OnHideAllSockets;
         }
     }
 }
