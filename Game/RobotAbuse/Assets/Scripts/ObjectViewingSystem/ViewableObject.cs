ae10c7d84f3b01489f85701105ee3e31f2491888
David <dgonzalez541@gmail.com>
2024-12-10 03:14:22 -0600

    Update ViewableObject.cs

--- Game/RobotAbuse/Assets/Scripts/ObjectViewingSystem/ViewableObject.cs
+++ Game/RobotAbuse/Assets/Scripts/ObjectViewingSystem/ViewableObject.cs
@@ -1,58 +1,48 @@
-
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace RobotAbuse
 {
-    [System.Serializable]
-    public class AssetReferenceMaterial : AssetReferenceT<Material>
-    {
-        public AssetReferenceMaterial(string guid) : base(guid) {}
-    }
-
+    //ViewableObject implements IViewableObject to be useable by ObejctViewer.
+    //ViewableObject can add interactions and features by implenting IHighlightable and ISocketable
     [DisallowMultipleComponent]
     public class ViewableObject : MonoBehaviour, IViewableObject, IHighlightable, ISocketable
     {
+        //IViewableObject
         [field: SerializeField] public GameObject[] AdditonalGameObjects { get; set; }
-        Dictionary<GameObject,Material> dictGameObjectMaterial = new Dictionary<GameObject,Material>();
 
-        [SerializeField] AssetReferenceMaterial materialReference;
+        //IHighlightable
         public bool IsHighlighted { get; private set; } = false;
 
-        public PartSocket PartSocket { get { return partSocket; } set { partSocket = value; } }
-        PartSocket partSocket;
-
+        [SerializeField] AssetReferenceMaterial materialReference;
+        Dictionary<GameObject,Material> dictGameObjectMaterial = new Dictionary<GameObject,Material>();
         Material highlightMaterial;
         Material originalMaterial;
         MeshRenderer meshRenderer;
+
+        //ISockatable
+        public PartSocket PartSocket { get { return partSocket; } set { partSocket = value; } }
+        PartSocket partSocket;
 
         void Awake()
         {
             meshRenderer = GetComponent<MeshRenderer>();
             originalMaterial = meshRenderer.material;
+
+            //Get materials from Addressables
             Addressables.LoadAssetsAsync<Material>(materialReference, (material) => 
             {
                 highlightMaterial = material;   
             });
 
+            //Get original materials from game objects
             foreach(GameObject go in AdditonalGameObjects)
             {
                 dictGameObjectMaterial.Add(go, go.GetComponent<MeshRenderer>().material);
             }
 
             partSocket = GetComponentInChildren<PartSocket>();
-            if(partSocket != null)
-            {
-                partSocket.OnSocketPartsConnected += PartSocket_OnTriggerEntered;
-            }
-
-        }
-
-        private void PartSocket_OnTriggerEntered(object sender, EventArgs e)
-        {
         }
 
         public void Highlight()
@@ -81,13 +71,11 @@ namespace RobotAbuse
                 
             }
         }
+    }
 
-        private void OnDisable()
-        {
-            if (partSocket != null)
-            {
-                partSocket.OnSocketPartsConnected -= PartSocket_OnTriggerEntered;
-            }
-        }
+    [System.Serializable]
+    public class AssetReferenceMaterial : AssetReferenceT<Material>
+    {
+        public AssetReferenceMaterial(string guid) : base(guid) { }
     }
 }
